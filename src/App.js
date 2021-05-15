038ff5ba4b28c6f21c731c6430d46331b42465ad
angshumanbhattacharjee <angshumanbhattacharjee80@gmail.com>
2021-05-15 14:22:37 +0530

    Completed Css of the weather app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import './App.css';
+import React, {useState} from 'react';
+
+const api = {
+  key: "f7292aaf3f1fbc92bcb5839b93c153ef",
+  base: "api.openweathermap.org/data/2.5/"
+}
 
 function App() {
+
+  
+
+  const dateBuilder = new Date().toDateString();
+
   return (
-    <div className="app warm">
+    <div className="app">
       <main>
         <div className="search-box">
         <input
@@ -10,6 +21,14 @@ function App() {
           type="text"
           placeholder="Search Location..."
         />
+        </div>
+        <div className="location-box">
+          <div className="location">Kolkata, India</div>
+          <div className="date">{dateBuilder}</div>
+        </div>
+        <div className="weather-box">
+          <div className="temp"> 20°C</div>
+          <div className="weather">Sunny</div>
         </div>
       </main>
     </div>
